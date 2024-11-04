716e33d0884616c77c7cc82c39b985224b804fe7
wilghelm <wilghelm@proton.me>
2024-11-04 17:06:37 +0200

    added order Entity

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use App\Http\Controllers\API\RegisterController;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\OrderController;
 
 Route::middleware('api')->group(function () {
     Route::get('/user', function (Request $request) {
@@ -13,5 +13,13 @@ Route::middleware('api')->group(function () {
     Route::controller(RegisterController::class)->group(function () {
         Route::post('register', 'register');
         Route::post('login', 'login');
+    });
+
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::get('orders', [OrderController::class, 'index']);
+        Route::post('orders', [OrderController::class, 'store']);
+        Route::get('orders/{id}', [OrderController::class, 'show']);
+        Route::put('orders/{id}', [OrderController::class, 'update']);
+        Route::delete('orders/{id}', [OrderController::class, 'destroy']);
     });
 });
